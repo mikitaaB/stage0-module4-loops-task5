2a1e4379a9bbc9bd8cb8f468e2cc3bff2d7f4403
Mikita Buinavets <mikita.buinavets@arua.co>
2024-02-26 00:31:37 +0300

    module 4. task 5

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,23 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= height; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+        for (int i = height - 1; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= height; j++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -1,7 +1,12 @@
 package school.mjc.stage0.loops.task5;
 
 public class Square {
-    public void printSquareFrom8s(int sideLength){
-
+    public void printSquareFrom8s(int sideLength) {
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++){
+                System.out.print("8");
+            }
+            System.out.println();
+        }
     }
 }
