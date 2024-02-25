e1e5d761966af488a2a8bf8d575caf6264210bb5
Mikita Buinavets <mikita.buinavets@arua.co>
2024-02-26 00:43:19 +0300

    module 4. task 5.fix

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,20 +2,21 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-        for (int i = 1; i <= height; i++) {
-            for (int j = 1; j < i; j++) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
-            for (int j = i; j <= height; j++) {
+            for (int j = 0; j < 2 * (height - i) - 1; j++) {
                 System.out.print("8");
             }
             System.out.println();
         }
-        for (int i = height - 1; i >= 1; i--) {
-            for (int j = 1; j < i; j++) {
+
+        for (int i = height - 2; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
-            for (int j = i; j <= height; j++) {
+            for (int j = 0; j < 2 * (height - i) - 1; j++) {
                 System.out.print("8");
             }
             System.out.println();
--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,9 +2,13 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength) {
-        for (int i = 0; i < sideLength; i++) {
-            for (int j = 0; j < sideLength; j++){
-                System.out.print("8");
+        for (int i = 1; i <= sideLength; i++) {
+            for (int j = 1; j <= sideLength; j++) {
+                if (i == 1 || i == sideLength || j == 1 || j == sideLength) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
