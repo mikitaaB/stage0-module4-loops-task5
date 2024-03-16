19469bea5b1ccbc7b0a9729e2a4e38805d07ad25
Mikita Buinavets <mikita.buinavets@arua.co>
2024-03-17 02:16:45 +0300

    hourglass. fix

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,26 +2,22 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int length) {
-        int height = (length + 1) / 2;
-
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < length; j++) {
-                if (j == 0 || j == length - 1 || i == height - 1) {
-                    System.out.print("8");
-                } else {
-                    System.out.print(" ");
-                }
+        for (int i = length; i > 0; i--) {
+            for (int j = length; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (2 * i - 1); j++) {
+                System.out.print("8");
             }
             System.out.println();
         }
 
-        for (int i = height - 2; i >= 0; i--) {
-            for (int j = 0; j < length; j++) {
-                if (j == 0 || j == length - 1 || i == 0) {
-                    System.out.print("8");
-                } else {
-                    System.out.print(" ");
-                }
+        for (int i = 2; i <= length; i++) {
+            for (int j = length; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (2 * i - 1); j++) {
+                System.out.print("8");
             }
             System.out.println();
         }
