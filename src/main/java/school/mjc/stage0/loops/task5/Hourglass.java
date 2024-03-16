62282d1933f056327fce69b650a330733d71cbca
Mikita Buinavets <mikita.buinavets@arua.co>
2024-03-17 02:49:23 +0300

    hourglass. fix 2

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,25 +1,35 @@
 package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
-    public void printHourglassOfGivenSize(int length) {
-        for (int i = length; i > 0; i--) {
-            for (int j = length; j > i; j--) {
+    public void printHourglassOfGivenSize(int n) {
+        int half = n / 2;
+        for (int i = half; i >= 0; i--) {
+            int base = n % 2 == 0 ? (2 * i) : (2 * i + 1);
+            for (int j = half; j > i; j--) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < (2 * i - 1); j++) {
+            for (int j = 0; j < base; j++) {
                 System.out.print("8");
             }
             System.out.println();
         }
 
-        for (int i = 2; i <= length; i++) {
-            for (int j = length; j > i; j--) {
+        for (int i = 1; i <= half; i++) {
+            int base = n % 2 == 0 ? (2 * i) : (2 * i + 1);
+            for (int j = half; j > i; j--) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < (2 * i - 1); j++) {
+            for (int j = 0; j < base; j++) {
                 System.out.print("8");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        Hourglass hourglassPrinter = new Hourglass();
+        hourglassPrinter.printHourglassOfGivenSize(6);
+        hourglassPrinter.printHourglassOfGivenSize(3);
+        hourglassPrinter.printHourglassOfGivenSize(5);
     }
 }
